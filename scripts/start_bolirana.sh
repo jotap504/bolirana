c60fc95fd54ee948b6f26cb61b98c0c5ae42dc92
Juan Pablo Pagotto <pagottojuanpablo@gmail.com>
2026-06-26 18:15:28 -0300

    fix(kiosk): remove disable-gpu and add no-sandbox for native chrome

--- scripts/start_bolirana.sh
+++ scripts/start_bolirana.sh
@@ -56,7 +56,7 @@ $CHROMIUM_BIN \
   --disable-infobars \
   --disable-session-crashed-bubble \
   --check-for-update-interval=31536000 \
-  --disable-gpu \
+  --no-sandbox \
   "http://127.0.0.1:8000/display/" &
 
 echo "=== Procesos de Bolirana iniciados con éxito ==="
